eb1af70f97f3529f78accd347f910484c1067474
hanumanAbacus <42829607+hanumanAbacus@users.noreply.github.com>
2021-06-30 22:37:55 +1000

    Update Reachability.m

--- Connectivity/Classes/Reachability/Reachability.m
+++ Connectivity/Classes/Reachability/Reachability.m
@@ -18,7 +18,7 @@
 //Reachability fully support IPv6.  For full details, see ReadMe.md.
 
 
-NSString *kReachabilityChangedNotification = @"kNetworkReachabilityChangedNotification";
+NSString *kABSReachabilityChangedNotification = @"kABSNetworkReachabilityChangedNotification";
 
 
 #pragma mark - Supporting functions
@@ -58,7 +58,7 @@ static void ReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkReach
 
     Reachability* noteObject = (__bridge Reachability *)info;
     // Post a notification to notify the client that the network reachability changed.
-    [[NSNotificationCenter defaultCenter] postNotificationName: kReachabilityChangedNotification object: noteObject];
+    [[NSNotificationCenter defaultCenter] postNotificationName: kABSReachabilityChangedNotification object: noteObject];
 }
 
 
